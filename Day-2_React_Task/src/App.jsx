42756919bb99bf9ec94e9c40584450edab2fdfcf
JayaPrakashSriram <sriram.fsd@gmail.com>
2023-03-30 05:55:49 +0530

    Component Created

---
+++ Day-2_React_Task/src/App.jsx
@@ -0,0 +1,62 @@
+export default function App(){
+  return(
+    <div>
+      <Pricingtable/>
+    </div>
+  )
+}
+
+function Pricingtable(){
+  return(
+    <div>
+    <div>
+       <li>Free</li>
+       <li>$0/month</li>
+       <ul>
+        <li>Single User </li>
+        <li>5GB Storage </li>
+        <li>Ultimated Public Project </li>
+        <li>Communtity Access</li>
+        <li>Unlimited Private Project </li>
+        <li>Dedicated Photo Support</li>
+        <li>Free Subdomain</li>
+        <li>Monthly Status Reports</li>
+       </ul>
+       <button>Button</button>
+    </div>
+    <div>
+    <li>Plus</li>
+    <li>$9/month</li>
+    <ul>
+     <li>5 Users </li>
+     <li>50GB Storage </li>
+     <li>Ultimated Public Project </li>
+     <li>Communtity Access</li>
+     <li>Unlimited Private Project </li>
+     <li>Dedicated Photo Support</li>
+     <li>Free Subdomain</li>
+     <li>Monthly Status Reports</li>
+    </ul>
+    <button>Button</button>
+ </div>
+ <div>
+       <li>Pro</li>
+       <li>$49/month</li>
+       <ul>
+        <li>Unlimited Users </li>
+        <li>150GB Storage </li>
+        <li>Ultimated Public Project </li>
+        <li>Communtity Access</li>
+        <li>Unlimited Private Project </li>
+        <li>Dedicated Photo Support</li>
+        <li>Unlimited Free Subdomains</li>
+        <li>Monthly Status Reports</li>
+        </ul>
+       <button>Button</button>
+    </div>
+ </div>
+  )
+}
+
+
+
